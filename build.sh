f565bb814fc781ecf7ea46675f051ce774b4fe01
Kevin King <4kevinking@gmail.com>
2017-02-26 12:00:18 -0500

    enable aarch64 android compilation

--- build.sh
+++ build.sh
@@ -9,3 +9,4 @@ cargo test
 cargo lipo --release
 #CC=gcc CXX=g++ cargo build --target armv7-linux-androideabi --release
 CC=gcc CXX=g++ cargo build --target arm-linux-androideabi --release
+CC=gcc CXX=g++ cargo build --target aarch64-linux-android --release
