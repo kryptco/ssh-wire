58da756b1eec797775d4e1d5fd682a370c002f6e
Kevin King <4kevinking@gmail.com>
2017-02-26 11:32:57 -0500

    fix android build

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ set -euo pipefail
 cargo test
 
 cargo lipo --release
-cargo build --target armv7-linux-androideabi --release
-cargo build --target arm-linux-androideabi --release
+#CC=gcc CXX=g++ cargo build --target armv7-linux-androideabi --release
+CC=gcc CXX=g++ cargo build --target arm-linux-androideabi --release
