9076ac718d8458a38c707dda1d80b791aaf58d51
Kevin King <4kevinking@gmail.com>
2017-03-15 13:56:17 -0400

    build armv7 android

--- build.sh
+++ build.sh
@@ -15,5 +15,6 @@ if [ "$?" != "0" ]; then
 	echo "ANDROID_NDK unset, skipping android compilation";
 else
 	PATH=$ANDROID_NDK/arm/bin/:$PATH CC=arm-linux-androideabi-gcc CXX=arm-linux-androideabi-g++ cargo build --target arm-linux-androideabi --release
+	PATH=$ANDROID_NDK/arm/bin/:$PATH CC=arm-linux-androideabi-gcc CXX=arm-linux-androideabi-g++ cargo build --target armv7-linux-androideabi --release
 	PATH=$ANDROID_NDK/arm64/bin/:$PATH CC=aarch64-linux-android-gcc CXX=aarch64-linux-android-g++ cargo build --target aarch64-linux-android --release
 fi
