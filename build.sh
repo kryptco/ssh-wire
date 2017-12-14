524ed010ed53d082b8facfe137bb84c9348464c9
Kevin King <kevin@krypt.co>
2017-12-14 17:06:09 -0500

    fix android compilation

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ cargo lipo --release --verbose
 if [ ${ANDROID_NDK:-"-"} = "-" ]; then
 	echo "ANDROID_NDK unset, skipping android compilation";
 else
-	PATH=$ANDROID_NDK/arm/bin/:$PATH CC=arm-linux-androideabi-gcc CXX=arm-linux-androideabi-g++ cargo build --target arm-linux-androideabi --release
-	PATH=$ANDROID_NDK/arm/bin/:$PATH CC=arm-linux-androideabi-gcc CXX=arm-linux-androideabi-g++ cargo build --target armv7-linux-androideabi --release
-	PATH=$ANDROID_NDK/arm64/bin/:$PATH CC=aarch64-linux-android-gcc CXX=aarch64-linux-android-g++ cargo build --target aarch64-linux-android --release
+	cargo build --target arm-linux-androideabi --release
+	cargo build --target armv7-linux-androideabi --release
+	cargo build --target aarch64-linux-android --release
 fi
